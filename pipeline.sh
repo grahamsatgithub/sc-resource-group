692f5731e24a81804df69e10fa6e6409e05aecd0
graham.north@viewdeck.com <graham.north@laptop-26.remote.viewdeck.com>
2020-02-28 15:36:04 +0000

    Add host share

--- pipeline.sh
+++ pipeline.sh
@@ -9,13 +9,33 @@ echo "configuration=$configuration"
 echo "entrypoint=$entrypoint"
 echo "container=$container"
 echo "hostmount=$hostmount"
+echo "hostshare=$hostshare"
 
 [[ -z "$configuration" ]] && exit
 [[ -z "$entrypoint" ]] && exit
 [[ -z "$container" ]] && exit
 [[ -z "$hostmount" ]] && exit
+[[ -z "$hostshare" ]] && exit 1
+
+cifsconfig="/etc/smbcredentials"
+cifscred="peith7teashoo7ab.cred"
+
+cifsshare="//peith7teashoo7ab.file.core.windows.net/pipeline"
+cifsmount="/mnt/peith7teashoo7ab"
+cifsoptions="vers=3.0,credentials=$cifsconfig/$cifscred,dir_mode=0777,file_mode=0777,serverino"
+
+sudo mkdir -vp "$cifsmount"
+sudo mkdir -vp "$cifsconfig"
+
+sudo mv -v "$cifscred" "$cifsconfig/$cifscred"
+
+sudo chown -v root:root "$cifsconfig/$cifscred"
+sudo chmod -v 600 "$cifsconfig/$cifscred"
+
+sudo mount -vt cifs "$cifsshare" "$cifsmount" -o "$cifsoptions" || exit 1
 
 docker run -i \
     -v "$PWD:$hostmount" \
+    -v "$cifsmount:$hostshare" \
     --entrypoint="$hostmount/$entrypoint" \
     "$container"
